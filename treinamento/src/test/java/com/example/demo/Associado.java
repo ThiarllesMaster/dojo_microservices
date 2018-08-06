2e1cf66d2af87c39208cb72817960e4e10f5f164
Gomes <thiarlles@gmail.com>
2018-08-06 18:12:05 -0300

    DOJO - MicroServices

---
+++ treinamento/src/test/java/com/example/demo/Associado.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+public class Associado{
+
+	private Long id;
+	private String nomeAssociado;
+	private String cpfAssociado;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNomeAssociado() {
+		return nomeAssociado;
+	}
+	public void setNomeAssociado(String nomeAssociado) {
+		this.nomeAssociado = nomeAssociado;
+	}
+	public String getCpfAssociado() {
+		return cpfAssociado;
+	}
+	public void setCpfAssociado(String cpfAssociado) {
+		this.cpfAssociado = cpfAssociado;
+	}
+	
+	
+}
--- treinamento/src/test/java/com/example/demo/TreinamentoApplicationTests.java
+++ treinamento/src/test/java/com/example/demo/TreinamentoApplicationTests.java
@@ -1,16 +1,67 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TreinamentoApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	static final String URL_SERVICE = "http://localhost:9080/associados";
+	final Integer CODE_SUCESS_POST = 201;
+	
+	//@Test
+	public void testMethodPost() throws JSONException {
+	    HttpHeaders httpHeaders = new HttpHeaders();
+	    httpHeaders.set("Content-Type", "application/json");
+	    JSONObject jsonObject = new JSONObject();
+	    jsonObject.put("id", 1L);
+	    jsonObject.put("nomeAssociado", "Thiarlles Duarte Gomes");
+	    jsonObject.put("cpfAssociado", "00840995008");
+	    
+	    HttpEntity<String> httpEntity = new HttpEntity <String> (jsonObject.toString(), httpHeaders);
+
+	    RestTemplate restTemplate = new RestTemplate();
+	    HttpStatus response = restTemplate.postForObject(URL_SERVICE, httpEntity, HttpStatus.class);
+	   
+	    assertTrue(response.value() == CODE_SUCESS_POST);
+        
 	}
+	
+	@Test 
+	public void testMethodGet() {
+		 RestTemplate restTemplate = new RestTemplate();
+		 ResponseEntity<List<Associado>> associadosRetornados =
+			        restTemplate.exchange(URL_SERVICE, HttpMethod.GET, null, new ParameterizedTypeReference<List<Associado>>() {
+					});
+	      
+		 List<Associado>associados = associadosRetornados.getBody();
+		 associados.forEach(a->System.out.println(a.getCpfAssociado()));
+		
+		
+		
+	}
+	
 
 }
+
+
