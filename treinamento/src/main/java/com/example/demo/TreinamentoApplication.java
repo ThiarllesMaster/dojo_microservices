a5ad69a7b1406e50047233ab695af7a0c003db10
thiarlles.a.gomes <thiarlles.a.gomes@CPX-8OUORYUEXD9.home>
2019-03-31 13:54:01 +0100

    Java 8

--- treinamento/src/main/java/com/example/demo/TreinamentoApplication.java
+++ treinamento/src/main/java/com/example/demo/TreinamentoApplication.java
@@ -1,5 +1,9 @@
 package com.example.demo;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -9,6 +13,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.Transactional;
 
 import domain.model.Associado;
 import domain.repository.AssociadoRepository;
@@ -18,27 +23,47 @@ import domain.repository.AssociadoRepository;
 @ComponentScan("domain.rest")
 @EnableJpaRepositories("domain.repository")
 public class TreinamentoApplication {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(TreinamentoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TreinamentoApplication.class, args);
 	}
-	
+
 	@Bean
 	public CommandLineRunner treinamento(AssociadoRepository associadoRepository) {
-		return(args)-> {
+		return (args) -> {
 			log.info("Saving the associado");
 			Associado associado = new Associado();
 			associado.setCpfAssociado("680.338.110-21");
 			associado.setNomeAssociado("Thiarlles Duarte Gomes");
 			associadoRepository.save(associado);
+			Associado associadoDois = new Associado();
+			associadoDois.setCpfAssociado("680.338.110-21");
+			associadoDois.setNomeAssociado("Thiarlles Duarte Gomes");
+			associadoRepository.save(associadoDois);
+
 			Associado optionalAssociado = associadoRepository.findById(associado.getId()).orElse(null);
-			Integer numberRecords = associadoRepository.countByNomeAssociado(associado.getNomeAssociado());
+			Integer numberRecords = associadoRepository.countByNomeAssociado(associado.getNomeAssociado() + "Teste");
+			List<Associado> associados = associadoRepository
+					.queryFirst1ByNomeAssociado(associadoDois.getNomeAssociado());
+
 			log.info("The associado's name {}", optionalAssociado.getNomeAssociado());
 			log.info("The associado's cpf {}", optionalAssociado.getCpfAssociado());
 			log.info("The count {}", numberRecords);
+			log.info("The size of list only 1 {}", associados.size());
+
+			findIds(associadoRepository);
 
 		};
 	}
+
+	
+	public List<Long> findIds(AssociadoRepository associadoRepository) {
+		return associadoRepository.findBynomeAssociado("Thiarlles Duarte Gomes").stream()
+				.filter(e -> !e.getCpfAssociado().isEmpty()).sorted(Comparator.comparing(Associado::getId).reversed())
+				.map(Associado::getId).collect(Collectors.toList());
+
+	}
+
 }
--- treinamento/src/main/java/domain/repository/AssociadoRepository.java
+++ treinamento/src/main/java/domain/repository/AssociadoRepository.java
@@ -5,17 +5,27 @@
 package domain.repository;
 
 import java.util.List;
+import java.util.stream.Stream;
+
+import javax.transaction.Transactional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
 import domain.model.Associado;
 
-@Repository
+
 public interface AssociadoRepository extends CrudRepository<Associado, Long>{
+	
 	
 	Integer countByNomeAssociado(String name);
 	
+	List<Associado>queryFirst1ByNomeAssociado(String nome);
+	
 	List<Associado>findByIdGreaterThan(Integer id);
+	
+	@org.springframework.transaction.annotation.Transactional(readOnly = true)
+	List<Associado>findBynomeAssociado(String name);
 
 }
